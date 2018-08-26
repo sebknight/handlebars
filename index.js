5f2a0d282bed4eab0da08605bdc577eadb1abce3
RichardHpaYoobee <richard.hpa@acgedu.com>
2018-08-27 09:44:39 +1200

    added handlebars into the project

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
 var app = express();
 const router = express.Router();
 
-
 const home = require('./routes/index');
 const about = require('./routes/about');
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', home);
 app.use('/about', about);
--- routes/about.js
+++ routes/about.js
@@ -2,15 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res){
-    res.end('about page');
+    res.render('about/index', {
+        title: 'about Page',
+        layout: 'secondary'
+    });
 });
 
 router.get('/us', function(req, res){
-    res.end('about Us');
+    res.render('about/us', {
+        title: 'about Us Page'
+    });
 })
 
 router.get('/product', function(req, res){
-    res.end('about the product');
+    res.render('about/product', {
+        title: 'about the product Page'
+    });
 })
 
 
